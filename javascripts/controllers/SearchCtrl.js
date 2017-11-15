742d071239016ffd6d67e9094c02afc22292576c
gib <gibbowden@gmail.com>
2017-11-14 19:12:55 -0600

    search

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,5 +1,13 @@
 "use strict"; 
 
-app.controller("SearchCtrl", function($scope){
-    $scope.controller = "SearchCtrl"; 
+app.controller("SearchCtrl", function($scope, tmdbService){
+    $scope.enterPush = (e) => {
+        if (e.keyCode === 13) {
+            tmdbService.searchMovies(e.target.value).then((results) => {
+                console.log(results.data.results);
+            }).catch((err) => {
+                console.log(err); 
+            });
+        }
+    }; 
 }); 
---
+++ javascripts/services/tmdbService.js
@@ -0,0 +1,8 @@
+"use strict"; 
+
+app.service("tmdbService", function($http, TMDB_CONFIG) {
+    const searchMovies = (searchString) => {
+        return $http.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_CONFIG}&language=en-US&page=1&include_adult=false&query=${searchString}`);
+    };
+    return {searchMovies}; 
+});
