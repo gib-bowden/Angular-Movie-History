43704dd8ea15c7635ad98f8754be5b203c4ab48f
gib <gibbowden@gmail.com>
2017-11-14 20:34:04 -0600

    posters are showing

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,10 +1,12 @@
 "use strict"; 
 
 app.controller("SearchCtrl", function($scope, tmdbService){
+    $scope.movies = []; 
+    
     $scope.enterPush = (e) => {
         if (e.keyCode === 13) {
             tmdbService.searchMovies(e.target.value).then((results) => {
-                console.log(results.data.results);
+                $scope.movies = results.data.results;
             }).catch((err) => {
                 console.log(err); 
             });
--- javascripts/services/tmdbService.js
+++ javascripts/services/tmdbService.js
@@ -4,5 +4,11 @@ app.service("tmdbService", function($http, TMDB_CONFIG) {
     const searchMovies = (searchString) => {
         return $http.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_CONFIG}&language=en-US&page=1&include_adult=false&query=${searchString}`);
     };
-    return {searchMovies}; 
+     
+
+    const tmdbConfiguration = () => {
+        return $http.get(`https://api.themoviedb.org/3/configuration?api_key=${TMDB_CONFIG}`);
+    };
+    return {searchMovies, tmdbConfiguration};
+    
 });
