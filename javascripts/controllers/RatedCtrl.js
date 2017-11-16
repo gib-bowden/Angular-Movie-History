64213a46e5c660b2e7a1c213714e8fa3b43a1549
gib <gibbowden@gmail.com>
2017-11-15 21:40:10 -0600

    delete is working

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -1,11 +1,25 @@
 "use strict"; 
 
 app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
-    $scope.movies = [];     
-    MovieService.getRatedMovies($rootScope.uid).then((results) => {
-        $scope.movies = results;
-        console.log(results);   
-    }).catch((err) => {
-        console.log(err); 
-    }); 
+    $scope.movies = [];  
+    
+    const getMovies = () => {
+        MovieService.getRatedMovies($rootScope.uid).then((results) => {
+            $scope.movies = results;
+            console.log(results);   
+        }).catch((err) => {
+            console.log(err); 
+        }); 
+    };
+
+    getMovies(); 
+
+    $scope.deleteMovie = (movieId) => {
+         MovieService.deleteMovie(movieId).then((result) => {
+            console.log(result); 
+            getMovies();  
+        }).catch((err) => {
+            console.log(err); 
+        });
+    };
 }); 
