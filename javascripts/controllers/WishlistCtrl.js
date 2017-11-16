094fd7c48a475771cc9fc2353b9bd571d6283877
gib <gibbowden@gmail.com>
2017-11-15 20:29:40 -0600

    wishlist is working

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -1,5 +1,11 @@
 "use strict"; 
 
-app.controller("WishlistCtrl", function($scope){
-    $scope.controller = "WishlistCtrl"; 
+app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
+    $scope.movies = [];     
+    MovieService.getWishlistMovies($rootScope.uid).then((results) => {
+        $scope.movies = results;
+        console.log(results);   
+    }).catch((err) => {
+        console.log(err); 
+    }); 
 }); 
