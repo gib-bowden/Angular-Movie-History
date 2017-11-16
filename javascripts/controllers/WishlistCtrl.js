a517f4827ac4e91f78e706e1881bb33d53cb8c52
gib <gibbowden@gmail.com>
2017-11-15 20:56:48 -0600

    rated button is adding to the database

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -4,7 +4,7 @@ app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
     $scope.movies = [];     
     MovieService.getWishlistMovies($rootScope.uid).then((results) => {
         $scope.movies = results;
-        console.log(results);   
+ 
     }).catch((err) => {
         console.log(err); 
     }); 
