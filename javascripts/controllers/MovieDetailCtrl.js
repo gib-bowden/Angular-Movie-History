d4172129607652bd18203e10311cf317e7f7eb19
gib <gibbowden@gmail.com>
2017-11-18 11:27:57 -0600

    hello_
    "
    
    git

--- javascripts/controllers/MovieDetailCtrl.js
+++ javascripts/controllers/MovieDetailCtrl.js
@@ -1,6 +1,41 @@
 "use strict"; 
 
-app.controller("MovieDetailCtrl", function($location, $rootScope, $scope, $window, AuthService){
-    $scope.hi = "hello";
+app.controller("MovieDetailCtrl", function($location, $rootScope, $routeParams, $scope, $window, MovieService){
+    
+    $scope.movie = {}; 
+
+    const getMovie = () => {
+        MovieService.getSingleMovie($routeParams.id).then((results) => {
+            $scope.movie = results.data; 
+        }).catch((err) => {
+            console.log(err); 
+        });
+    };
+
+    getMovie(); 
+
+
+    $scope.switchWatched = (movie) => {
+        movie.isWatched = true;
+        let updatedMovie = MovieService.createMovieObject(movie);
+        MovieService.updateMovie(updatedMovie, $routeParams.id).then((result) => {
+            console.log(result); 
+            getMovie(); 
+        }).catch((err) => {
+            console.log(err); 
+        });
+    };
+
+    $scope.starChange = (event, movie) => {
+        if (event.rating) {
+            movie.rating = event.rating;
+            let updatedMovie = MovieService.createMovieObject(movie);
+            MovieService.updateMovie(updatedMovie, $routeParams.id).then(() => {
+                getMovie(); 
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    };
 
 }); 
